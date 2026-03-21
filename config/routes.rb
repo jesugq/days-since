173cf893881de76b6fa6aae160af2fbb5bef5b1e
jesugq <jesugq@outlook.com>
2026-03-20 19:29:26 -0600

    Add event controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :events, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
