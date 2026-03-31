d9b0e2d619c28ab61060972b2e0e8ecf48c4b9cf
jesugq <jesugq@outlook.com>
2026-03-30 20:32:52 -0600

    Migrate tests to rspec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,61 +1,61 @@
-require "test_helper"
+require "rails_helper"
 
-class EventTest < ActiveSupport::TestCase
-  test "should be valid with all attributes" do
+RSpec.describe Event do
+  it "is valid with valid attributes" do
     event = Event.new(
       name: "Test Event",
       description: "Test Description",
       occurred_on: Date.today,
       slug: "test-event"
     )
-    assert event.valid?
+    expect(event).to be_valid
   end
 
-  test "should not be valid without a name" do
+  it "is not valid without a name" do
     event = Event.new(
       description: "Test Description",
       occurred_on: Date.today,
       slug: "test-event"
     )
-    assert_not event.valid?
+    expect(event).to be_invalid
   end
   
-  test "should not be valid without a description" do
+  it "is not valid without a description" do
     event = Event.new(
       name: "Test Event",
       occurred_on: Date.today,
       slug: "test-event"
     )
-    assert_not event.valid?
+    expect(event).to be_invalid
   end
   
-  test "should not be valid without a occurred_on" do
+  it "is not valid without a occurred_on" do
     event = Event.new(
       name: "Test Event",
       description: "Test Description",
       slug: "test-event"
     )
-    assert_not event.valid?
+    expect(event).to be_invalid
   end
   
-  test "should not be valid without a slug" do
+  it "is not valid without a slug" do
     event = Event.new(
       name: "Test Event",
       description: "Test Description",
       occurred_on: Date.today
     )
-    assert_not event.valid?
+    expect(event).to be_invalid
   end
   
-  test "should not be valid with a duplicate slug" do
+  it "is not valid with a duplicate slug" do
     event_0 = Event.new(
       name: "Test Event",
       description: "Test Description",
       occurred_on: Date.today,
       slug: "duplicate-event"
     )
-    assert event_0.valid?
-    assert event_0.save
+    expect(event_0).to be_valid
+    expect(event_0.save).to be_truthy
 
     event_1 = Event.new(
       name: "Test Event",
@@ -63,7 +63,7 @@ class EventTest < ActiveSupport::TestCase
       occurred_on: Date.today,
       slug: "duplicate-event"
     )
-    assert_not event_1.valid?
-    assert_not event_1.save
+    expect(event_1).to be_invalid
+    expect(event_1.errors.full_messages).to include("Slug has already been taken")
   end
 end
